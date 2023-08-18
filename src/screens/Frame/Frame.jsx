fb4738be65516292bcc404c96c325f512f1d7d1e
Shalom Harpaz <111239198+sh770@users.noreply.github.com>
2023-08-18 16:30:49 +0300

    Initial commit

---
+++ src/screens/Frame/Frame.jsx
@@ -0,0 +1,141 @@
+import React from "react";
+import "./style.css";
+
+export const Frame = () => {
+  return (
+    <div className="frame">
+      <div className="div">
+        <div className="overlap-group">
+          <div className="view" />
+          <div className="view-2" />
+          <div className="view-3" />
+          <p className="text-wrapper">
+            לורם איפסום דולור סיט אמט, <br />
+            קונסקטורר אדיפיסינג אלית הועניב <br />
+            היושבב שערש שמחויט - שלושע <br />
+            ותלברו חשלו שעותלשך וחאית <br />
+            נובש ערששף
+          </p>
+          <div className="text-wrapper-2">כותרת ראשית</div>
+          <img
+            className="image"
+            alt="Image"
+            src="https://generation-sessions.s3.amazonaws.com/5e2ef2a375cae7f356c69b1e8389036e/img/-----------.png"
+          />
+          <div className="text-wrapper-3">הרשמה</div>
+          <div className="text-wrapper-4">התחברות</div>
+          <div className="group" />
+          <div className="rectangle" />
+          <div className="text-wrapper-5">להורדה</div>
+          <div className="rectangle-2" />
+        </div>
+        <div className="overlap">
+          <img
+            className="img"
+            alt="Frame"
+            src="https://generation-sessions.s3.amazonaws.com/5e2ef2a375cae7f356c69b1e8389036e/img/frame-4.png"
+          />
+          <img
+            className="img-2"
+            alt="Frame"
+            src="https://generation-sessions.s3.amazonaws.com/5e2ef2a375cae7f356c69b1e8389036e/img/frame-5.png"
+          />
+          <img
+            className="img-3"
+            alt="Frame"
+            src="https://generation-sessions.s3.amazonaws.com/5e2ef2a375cae7f356c69b1e8389036e/img/frame-4.png"
+          />
+          <img
+            className="img-4"
+            alt="Frame"
+            src="https://generation-sessions.s3.amazonaws.com/5e2ef2a375cae7f356c69b1e8389036e/img/frame-5.png"
+          />
+        </div>
+        <div className="frame-wrapper">
+          <div className="div-2">
+            <div className="component">
+              <p className="TITLE">
+                <span className="span">
+                  TITLE
+                  <br />
+                  <br />
+                </span>
+                <span className="text-wrapper-6">
+                  רם איפסום דולור סיט אמט,  קונסקטורר אדיפיסינג
+                  <br />
+                  אלית הועניב{" "}
+                </span>
+              </p>
+              <img
+                className="ellipse"
+                alt="Ellipse"
+                src="https://generation-sessions.s3.amazonaws.com/5e2ef2a375cae7f356c69b1e8389036e/img/ellipse-3-3.png"
+              />
+            </div>
+            <div className="component">
+              <p className="TITLE">
+                <span className="span">
+                  TITLE
+                  <br />
+                  <br />
+                </span>
+                <span className="text-wrapper-6">
+                  רם איפסום דולור סיט אמט,  קונסקטורר אדיפיסינג
+                  <br />
+                  אלית הועניב{" "}
+                </span>
+              </p>
+              <img
+                className="ellipse"
+                alt="Ellipse"
+                src="https://generation-sessions.s3.amazonaws.com/5e2ef2a375cae7f356c69b1e8389036e/img/ellipse-3-2.svg"
+              />
+            </div>
+            <div className="component">
+              <p className="TITLE">
+                <span className="span">
+                  TITLE
+                  <br />
+                  <br />
+                </span>
+                <span className="text-wrapper-6">
+                  רם איפסום דולור סיט אמט,  קונסקטורר אדיפיסינג
+                  <br />
+                  אלית הועניב{" "}
+                </span>
+              </p>
+              <img
+                className="ellipse"
+                alt="Ellipse"
+                src="https://generation-sessions.s3.amazonaws.com/5e2ef2a375cae7f356c69b1e8389036e/img/ellipse-3-1.svg"
+              />
+            </div>
+            <div className="component">
+              <p className="TITLE">
+                <span className="span">
+                  TITLE
+                  <br />
+                  <br />
+                </span>
+                <span className="text-wrapper-6">
+                  רם איפסום דולור סיט אמט,  קונסקטורר אדיפיסינג
+                  <br />
+                  אלית הועניב{" "}
+                </span>
+              </p>
+              <img
+                className="ellipse"
+                alt="Ellipse"
+                src="https://generation-sessions.s3.amazonaws.com/5e2ef2a375cae7f356c69b1e8389036e/img/ellipse-3.svg"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="overlap-2">
+          <div className="rectangle-3" />
+          <div className="text-wrapper-7">פוטר</div>
+        </div>
+      </div>
+    </div>
+  );
+};
